ad9f93425103082ff9314ad8707b77c716ce2a1f
Aditya Nath <84228628+MrEvilGamer@users.noreply.github.com>
2021-07-31 23:08:44 +0530

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,17 @@
+fx_version 'cerulean'
+
+game 'gta5'
+
+description 'NTRP-FRAGWEED'
+
+server_scripts {
+	'config.lua',
+	'server/weedplant.lua'
+}
+
+client_scripts {
+	'config.lua',
+	'client/weedplant.lua'
+}
+
+shared_script '@qb-core/import.lua'
