b15ba8c4e59220bef7ed57e3a02d93c13170ed46
Aditya Nath <84228628+MrEvilGamer@users.noreply.github.com>
2021-08-01 13:06:04 +0530

    Update shared.lua

--- shared.lua
+++ shared.lua
@@ -1,5 +1,7 @@
 -- Add this in Shared.lua of qb-core/shard.lua
 
 
-["cannabis"] 			 		 = {["name"] = "cannabis", 			 	        ["label"] = "Cannabis", 			    ["weight"] = 200, 		["type"] = "item", 		["image"] = "cannabis.png", 			["unique"] = false, 	["useable"] = true, 	["shouldClose"] = true,    ["combinable"] = nil,   ["description"] = "A Road To Happieness!!"},
-["marijuana"] 					 = {["name"] = "marijuana", 			  	  	["label"] = "Bag of Marijuana", 		["weight"] = 0, 		["type"] = "item", 		["image"] = "weed-baggie.png", 		    ["unique"] = false, 	["useable"] = true, 	["shouldClose"] = true,    ["combinable"] = nil,   ["description"] = "To get happy real fast."},
+["cannabis"] 			 		 = {["name"] = "cannabis", 			 	    ["label"] = "Cannabis", 		    ["weight"] = 200, 		["type"] = "item", 		["image"] = "cannabis.png", 			["unique"] = false, 	["useable"] = true, 	["shouldClose"] = true,    ["combinable"] = nil,   ["description"] = "A Road To Happieness!!"},
+["empty_weed_bag"] 				 = {["name"] = "empty_weed_bag", 			["label"] = "Empty Weed Bag", 		["weight"] = 10, 		["type"] = "item", 		["image"] = "empty-weed-baggie.png", 	["unique"] = false, 	["useable"] = true, 	["shouldClose"] = true,    ["combinable"] = nil,   ["description"] = "To get happy real fast."},
+["weed_bag"] 					 = {["name"] = "weed_bag", 			  	  	["label"] = "Weed Bag", 		    ["weight"] = 0, 		["type"] = "item", 		["image"] = "weed-baggie.png", 		    ["unique"] = false, 	["useable"] = true, 	["shouldClose"] = true,    ["combinable"] = nil,   ["description"] = "To get happy real fast."},
+
