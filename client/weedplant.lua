d8de7b518746634351f90f6c0f662fe73416ff63
Aditya Nath <84228628+MrEvilGamer@users.noreply.github.com>
2021-08-02 14:33:44 +0530

    Add files via upload

---
+++ client/weedplant.lua
@@ -0,0 +1,346 @@
+isLoggedIn = true
+
+local menuOpen = false
+local wasOpen = false
+local spawnedWeed = 0
+local weedPlants = {}
+
+local isPickingUp, isProcessing, isProcessing2 = false, false, false
+
+RegisterNetEvent("QBCore:Client:OnPlayerLoaded")
+AddEventHandler("QBCore:Client:OnPlayerLoaded", function()
+	CheckCoords()
+	Citizen.Wait(1000)
+	local coords = GetEntityCoords(PlayerPedId())
+	if GetDistanceBetweenCoords(coords, Config.CircleZones.WeedField.coords, true) < 1000 then
+		SpawnWeedPlants()
+	end
+end)
+
+function CheckCoords()
+	Citizen.CreateThread(function()
+		while true do
+			local coords = GetEntityCoords(PlayerPedId())
+			if GetDistanceBetweenCoords(coords, Config.CircleZones.WeedField.coords, true) < 1000 then
+				SpawnWeedPlants()
+			end
+			Citizen.Wait(1 * 60000)
+		end
+	end)
+end
+
+AddEventHandler('onResourceStart', function(resource)
+	if resource == GetCurrentResourceName() then
+		CheckCoords()
+	end
+end)
+Citizen.CreateThread(function()--weed
+	while true do
+		Citizen.Wait(10)
+
+		local playerPed = PlayerPedId()
+		local coords = GetEntityCoords(playerPed)
+		local nearbyObject, nearbyID
+		
+		
+		for i=1, #weedPlants, 1 do
+			if GetDistanceBetweenCoords(coords, GetEntityCoords(weedPlants[i]), false) < 1 then
+				nearbyObject, nearbyID = weedPlants[i], i
+			end
+		end
+
+		if nearbyObject and IsPedOnFoot(playerPed) then
+
+			if not isPickingUp then
+				QBCore.Functions.Draw2DText(0.5, 0.88, 'Press ~g~[E]~w~ to pickup Cannabis', 0.4)
+			end
+
+			if IsControlJustReleased(0, 38) and not isPickingUp then
+				isPickingUp = true
+				TaskStartScenarioInPlace(playerPed, 'world_human_gardener_plant', 0, false)
+				QBCore.Functions.Progressbar("search_register", "Picking up Cannabis..", 3000, false, true, {
+					disableMovement = true,
+					disableCarMovement = true,
+					disableMouse = false,
+					disableCombat = true,
+					disableInventory = true,
+				}, {}, {}, {}, function() -- Done
+					ClearPedTasks(PlayerPedId())
+					QBCore.Functions.DeleteObject(nearbyObject)
+
+					table.remove(weedPlants, nearbyID)
+					spawnedWeed = spawnedWeed - 1
+
+					TriggerServerEvent('qb-weedpicking:pickedUpCannabis')
+				end, function()
+					ClearPedTasks(PlayerPedId())
+				end)
+
+				isPickingUp = false
+			end
+		else
+			Citizen.Wait(500)
+		end
+	end
+end)
+
+AddEventHandler('onResourceStop', function(resource)
+	if resource == GetCurrentResourceName() then
+		for k, v in pairs(weedPlants) do
+			QBCore.Functions.DeleteObject(v)
+		end
+	end
+end)
+function SpawnWeedPlants()
+	while spawnedWeed < 20 do
+		Citizen.Wait(1)
+		local weedCoords = GenerateWeedCoords()
+
+		QBCore.Functions.SpawnLocalObject('prop_weed_02', weedCoords, function(obj)
+			PlaceObjectOnGroundProperly(obj)
+			FreezeEntityPosition(obj, true)
+			
+
+			table.insert(weedPlants, obj)
+			spawnedWeed = spawnedWeed + 1
+		end)
+	end
+	Citizen.Wait(45 * 60000)
+end
+
+function ValidateWeedCoord(plantCoord)
+	if spawnedWeed > 0 then
+		local validate = true
+
+		for k, v in pairs(weedPlants) do
+			if GetDistanceBetweenCoords(plantCoord, GetEntityCoords(v), true) < 5 then
+				validate = false
+			end
+		end
+
+		if GetDistanceBetweenCoords(plantCoord, Config.CircleZones.WeedField.coords, false) > 50 then
+			validate = false
+		end
+
+		return validate
+	else
+		return true
+	end
+end
+
+function GenerateWeedCoords()
+	while true do
+		Citizen.Wait(1)
+
+		local weedCoordX, weedCoordY
+
+		math.randomseed(GetGameTimer())
+		local modX = math.random(-10, 10)
+
+		Citizen.Wait(100)
+
+		math.randomseed(GetGameTimer())
+		local modY = math.random(-10, 10)
+
+		weedCoordX = Config.CircleZones.WeedField.coords.x + modX
+		weedCoordY = Config.CircleZones.WeedField.coords.y + modY
+
+		local coordZ = GetCoordZWeed(weedCoordX, weedCoordY)
+		local coord = vector3(weedCoordX, weedCoordY, coordZ)
+
+		if ValidateWeedCoord(coord) then
+			return coord
+		end
+	end
+end
+
+function GetCoordZWeed(x, y)
+	local groundCheckHeights = { 31.0, 32.0, 33.0, 34.0, 35.0, 36.0, 37.0, 38.0, 39.0, 40.0, 50.0 }
+
+	for i, height in ipairs(groundCheckHeights) do
+		local foundGround, z = GetGroundZFor_3dCoord(x, y, height)
+
+		if foundGround then
+			return z
+		end
+	end
+
+	return 31.85
+end
+
+Citizen.CreateThread(function()
+	while QBCore == nil do
+		Citizen.Wait(200)
+	end
+	while true do
+		Citizen.Wait(10)
+		local playerPed = PlayerPedId()
+		local coords = GetEntityCoords(playerPed)
+
+		if GetDistanceBetweenCoords(coords, Config.CircleZones.WeedProcessing.coords, true) < 5 then
+			DrawMarker(2, Config.CircleZones.WeedProcessing.coords.x, Config.CircleZones.WeedProcessing.coords.y, Config.CircleZones.WeedProcessing.coords.z - 0.2 , 0, 0, 0, 0, 0, 0, 0.3, 0.2, 0.15, 255, 0, 0, 100, 0, 0, 0, true, 0, 0, 0)
+
+			
+			if not isProcessing and GetDistanceBetweenCoords(coords, Config.CircleZones.WeedProcessing.coords, true) <1 then
+				QBCore.Functions.DrawText3D(Config.CircleZones.WeedProcessing.coords.x, Config.CircleZones.WeedProcessing.coords.y, Config.CircleZones.WeedProcessing.coords.z, 'Press ~g~[E]~w~ to Process')
+			end
+
+			if IsControlJustReleased(0, 38) and not isProcessing then
+				local hasBag = false
+				local s1 = false
+				local hasWeed = false
+				local s2 = false
+
+				QBCore.Functions.TriggerCallback('QBCore:HasItem', function(result)
+					hasWeed = result
+					s1 = true
+				end, 'cannabis')
+				
+				while(not s1) do
+					Citizen.Wait(100)
+				end
+				Citizen.Wait(100)
+				QBCore.Functions.TriggerCallback('QBCore:HasItem', function(result)
+					hasBag = result
+					s2 = true
+				end, 'empty_weed_bag')
+				
+				while(not s2) do
+					Citizen.Wait(100)
+				end
+
+				if (hasWeed and hasBag) then
+					Processweed()
+				elseif (hasWeed) then
+					QBCore.Functions.Notify('You dont have enough plastic bags.', 'error')
+				elseif (hasBag) then
+					QBCore.Functions.Notify('You dont have enough cannabis.', 'error')
+				else
+					QBCore.Functions.Notify('You dont have enough cannabis and plastic bags.', 'error')
+				end
+			end
+		else
+			Citizen.Wait(500)
+		end
+	end
+end)
+
+function Processweed()
+	isProcessing = true
+	local playerPed = PlayerPedId()
+
+	TaskStartScenarioInPlace(playerPed, "PROP_HUMAN_PARKING_METER", 0, true)
+	SetEntityHeading(PlayerPedId(), 108.06254)
+
+	QBCore.Functions.Progressbar("search_register", "Trying to Process..", 30000, false, true, {
+		disableMovement = true,
+		disableCarMovement = true,
+		disableMouse = false,
+		disableCombat = true,
+		disableInventory = true,
+	}, {}, {}, {}, function()
+	 TriggerServerEvent('qb-weedpicking:processweed')
+
+		local timeLeft = Config.Delays.WeedProcessing / 1000
+
+		while timeLeft > 0 do
+			Citizen.Wait(1000)
+			timeLeft = timeLeft - 1
+
+			if GetDistanceBetweenCoords(GetEntityCoords(playerPed), Config.CircleZones.WeedProcessing.coords, false) > 4 then
+				TriggerServerEvent('qb-weedpicking:cancelProcessing')
+				break
+			end
+		end
+		ClearPedTasks(PlayerPedId())
+	end, function()
+		ClearPedTasks(PlayerPedId())
+	end) -- Cancel
+	
+	isProcessing = false
+end
+
+Citizen.CreateThread(function()
+	while QBCore == nil do
+		Citizen.Wait(200)
+	end
+	while true do
+		Citizen.Wait(10)
+		local playerPed = PlayerPedId()
+		local coords = GetEntityCoords(playerPed)
+
+		if GetDistanceBetweenCoords(coords, Config.CircleZones.DrugDealer.coords, true) < 5 then
+			DrawMarker(2, Config.CircleZones.DrugDealer.coords.x, Config.CircleZones.DrugDealer.coords.y, Config.CircleZones.DrugDealer.coords.z - 0.2 , 0, 0, 0, 0, 0, 0, 0.3, 0.2, 0.15, 255, 0, 0, 100, 0, 0, 0, true, 0, 0, 0)
+
+			
+			if not isProcessing2 and GetDistanceBetweenCoords(coords, Config.CircleZones.DrugDealer.coords, true) <1 then
+				QBCore.Functions.DrawText3D(Config.CircleZones.DrugDealer.coords.x, Config.CircleZones.DrugDealer.coords.y, Config.CircleZones.DrugDealer.coords.z, 'Press ~g~[E]~w~ to Sell')
+			end
+
+			if IsControlJustReleased(0, 38) and not isProcessing2 then
+				local hasWeed2 = false
+				local hasBag2 = false
+				local s3 = false
+				
+				QBCore.Functions.TriggerCallback('QBCore:HasItem', function(result)
+					hasWeed2 = result
+					hasBag2 = result
+					s3 = true
+					
+				end, 'weed_bag')
+				
+				while(not s3) do
+					Citizen.Wait(100)
+				end
+				
+
+				if (hasWeed2) then
+					SellDrug()
+				elseif (hasWeed2) then
+					QBCore.Functions.Notify('You dont have enough plastic bags.', 'error')
+				elseif (hasBag2) then
+					QBCore.Functions.Notify('You dont have enough cannabis.', 'error')
+				else
+					QBCore.Functions.Notify('You dont have enough weed bags to sell.', 'error')
+				end
+			end
+		else
+			Citizen.Wait(500)
+		end
+	end
+end)
+
+function SellDrug()
+	isProcessing2 = true
+	local playerPed = PlayerPedId()
+
+	--
+	TaskStartScenarioInPlace(playerPed, "PROP_HUMAN_PARKING_METER", 0, true)
+	SetEntityHeading(PlayerPedId(), 108.06254)
+
+	QBCore.Functions.Progressbar("search_register", "Trying to Sell..", 1500, false, true, {
+		disableMovement = true,
+		disableCarMovement = true,
+		disableMouse = false,
+		disableCombat = true,
+		disableInventory = true,
+	}, {}, {}, {}, function()
+	 TriggerServerEvent('qb-weedpicking:selld')
+
+		local timeLeft = Config.Delays.WeedProcessing / 1000
+
+		while timeLeft > 0 do
+			Citizen.Wait(500)
+			timeLeft = timeLeft - 1
+
+			if GetDistanceBetweenCoords(GetEntityCoords(playerPed), Config.CircleZones.WeedProcessing.coords, false) > 4 then
+				break
+			end
+		end
+		ClearPedTasks(PlayerPedId())
+	end, function()
+		ClearPedTasks(PlayerPedId())
+	end) -- Cancel
+
+	isProcessing2 = false
+end
---
+++ server/weedplant.lua
@@ -0,0 +1,101 @@
+local itemcraft = 'black_money'
+
+RegisterServerEvent('qb-weedpicking:pickedUpCannabis') --hero
+AddEventHandler('qb-weedpicking:pickedUpCannabis', function()
+	local src = source
+	local Player = QBCore.Functions.GetPlayer(src)
+
+	    if 	TriggerClientEvent("QBCore:Notify", src, "Picked up some Cannabis!!", "Success", 1000) then
+		  Player.Functions.AddItem('cannabis', 1) ---- change this shit 
+		  TriggerClientEvent("inventory:client:ItemBox", source, QBCore.Shared.Items['cannabis'], "add")
+	    end
+end)
+
+RegisterServerEvent('qb-weedpicking:processweed')
+AddEventHandler('qb-weedpicking:processweed', function()
+    local src = source
+    local Player = QBCore.Functions.GetPlayer(src)
+    local cannabis = Player.Functions.GetItemByName("cannabis")
+      local empty_weed_bag = Player.Functions.GetItemByName("empty_weed_bag")
+
+    if cannabis ~= nil and empty_weed_bag ~= nil then
+        if Player.Functions.RemoveItem('cannabis', 3) and Player.Functions.RemoveItem('empty_weed_bag', 1) then
+            Player.Functions.AddItem('weed_bag', 1)-----change this
+            TriggerClientEvent("inventory:client:ItemBox", source, QBCore.Shared.Items['cannabis'], "remove")
+            TriggerClientEvent("inventory:client:ItemBox", source, QBCore.Shared.Items['empty_weed_bag'], "remove")
+            TriggerClientEvent("inventory:client:ItemBox", source, QBCore.Shared.Items['weed_bag'], "add")
+            TriggerClientEvent('QBCore:Notify', src, 'Cannabis Processed successfully', "success")  
+        else
+            TriggerClientEvent('QBCore:Notify', src, 'You don\'t have the right items', "error") 
+        end
+    else
+        TriggerClientEvent('QBCore:Notify', src, 'You don\'t have the right items', "error") 
+    end
+end)
+
+--selldrug ok
+
+RegisterServerEvent('qb-weedpicking:selld')
+AddEventHandler('qb-weedpicking:selld', function()
+	local src = source
+	local Player = QBCore.Functions.GetPlayer(src)
+	local Item = Player.Functions.GetItemByName('weed_bag')
+   
+	if Item ~= nil and Item.amount >= 1 then
+		local chance2 = math.random(1, 12)
+		if chance2 == 1 or chance2 == 2 or chance2 == 9 or chance2 == 4 or chance2 == 10 or chance2 == 6 or chance2 == 7 or chance2 == 8 then
+			Player.Functions.RemoveItem('weed_bag', 1)----change this
+			TriggerClientEvent("inventory:client:ItemBox", source, QBCore.Shared.Items['weed_bag'], "remove")
+			Player.Functions.AddMoney("cash", Config.Pricesell, "sold-pawn-items")
+			TriggerClientEvent('QBCore:Notify', src, 'you sold to the pusher', "success")  
+		else
+			Player.Functions.RemoveItem('weed_bag', 1)----change this
+			TriggerClientEvent("inventory:client:ItemBox", source, QBCore.Shared.Items['weed_bag'], "remove")
+			Player.Functions.AddMoney("cash", Config.Pricesell-100, "sold-pawn-items")
+			TriggerClientEvent('QBCore:Notify', src, 'you sold to the pusher', "success")
+		end
+else
+	TriggerClientEvent('QBCore:Notify', src, 'You don\'t have the right items', "error") 
+	
+end
+end)
+
+function CancelProcessing(playerId)
+	if playersProcessingCannabis[playerId] then
+		ClearTimeout(playersProcessingCannabis[playerId])
+		playersProcessingCannabis[playerId] = nil
+	end
+end
+
+RegisterServerEvent('qb-weedpicking:cancelProcessing')
+AddEventHandler('qb-weedpicking:cancelProcessing', function()
+	CancelProcessing(source)
+end)
+
+AddEventHandler('QBCore_:playerDropped', function(playerId, reason)
+	CancelProcessing(playerId)
+end)
+
+RegisterServerEvent('qb-weedpicking:onPlayerDeath')
+AddEventHandler('qb-weedpicking:onPlayerDeath', function(data)
+	local src = source
+	CancelProcessing(src)
+end)
+
+QBCore.Functions.CreateCallback('poppy:process', function(source, cb)
+	local src = source
+	local Player = QBCore.Functions.GetPlayer(src)
+	 
+	if Player.PlayerData.items ~= nil and next(Player.PlayerData.items) ~= nil then
+		for k, v in pairs(Player.PlayerData.items) do
+		    if Player.Playerdata.items[k] ~= nil then
+				if Player.Playerdata.items[k].name == "cannabis" then
+					cb(true)
+			    else
+					TriggerClientEvent("QBCore:Notify", src, "You do not have any Cannabis", "error", 10000)
+					cb(false)
+				end
+	        end
+		end	
+	end
+end)
